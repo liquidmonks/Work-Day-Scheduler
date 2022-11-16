09b1b34ae3e80b639d3091253140e26d705526b9
Liquid Monks <info@liquidmonks.com>
2022-11-15 20:29:45 -0600

    script.js includes a global variable set that uses jQuery to pull the current date and time when the user loads the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Global variables that define the current day and sets the time format
+
+let currentTime = $("#currentDay");
+let currentHour = moment().format("H");
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
