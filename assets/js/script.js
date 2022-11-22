e279df2f00c543dcc771c8ffe7a525b4ed2b4950
Liquid Monks <info@liquidmonks.com>
2022-11-22 08:54:13 -0600

    script.js includes conditional statement that compares the current date with the local storage. If these two paramaters are equal, it applies the saved values to the time blocks. If they are not equal, it clears the local storage and saves the current date in the local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,6 @@
 let currentDate = dayjs().format("dddd-MMMM-YYYY HH:mm:ss");
 $("#currentDay").text(currentDate);
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
 $(function () {
   let buttons = $(".saveBtn");
 
@@ -21,26 +14,28 @@ $(function () {
     localStorage.setItem(parent, description);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  // The code needs to remove all past, present and future classes if already present
+  // Removes all past, present and future classes if already present
   $(function () {
     $(".time-block").removeClass("past");
     $(".time-block").removeClass("present");
     $(".time-block").removeClass("future");
-    // The codes needs to compare the current hour with the ids of the elements which are numbered according to the hours.
+
+    // Compares the current hour with the ids of the elements which are numbered according to the hours.
+
     $(".time-block").each(function () {
       let id = $(this).attr("id");
       let currentHour = Number(dayjs().format("HH"));
       let split_id = id.split("-");
       let id_hr = Number(split_id[1]);
 
-      /*  if a time-block has an hour smaller than the current hour, the past class will be alloted to it. And if it is equal to the current
-  hour, the present class will be given. And if the hour is greater than the current hour, the future class will be given. */
+      /*  
+  ***********************************************************************************************
+  if a time-block has an hour smaller than the current hour, the past class will be alloted to it. 
+  ***********************************************************************************************
+  And if it is equal to the current hour, the present class will be given. 
+  ***********************************************************************************************
+  And if the hour is greater than the current hour, the future class will be given. 
+    */
       if (id_hr < currentHour) {
         $(this).addClass("past");
       } else if (id_hr == currentHour) {
@@ -51,15 +46,29 @@ $(function () {
     });
   });
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  /* Function to reset or apply all the saved values to time blocks. */
 
-  //  Function to reset or apply all the saved values to time blocks.
-
-  // Will take the current date in DD-MM-YYYY format.
+  // Takes the current date in DD-MM-YYYY format.
   let currentDay = dayjs().format("DD-MM-YYYY");
 
-  // TODO: Add code to display the current date in the header of the page.
+  // Takes the current date in DD-MM-YYYY format from local storage.
+  let savedDay = localStorage.getItem("day");
+
+  // Compares the current date with the date in local storage. If they are equal, the saved values will be applied to the time blocks.
+  // If they are not equal, the local storage will be cleared and the current date will be saved in local storage.
+
+  if (savedDay != null && savedDay != currentDay) {
+    $(".time-block").each(function () {
+      let id = $(this).attr("id");
+      let data = localStorage.getItem(id);
+      $("#" + id + " .description").val(data);
+    });
+  } else {
+    $(".time-block").each(function () {
+      let id = $(this).attr("id");
+      $(id).val("");
+      localStorage.setItem("day", currentDay);
+      localStorage.setItem(id, "");
+    });
+  }
 });
